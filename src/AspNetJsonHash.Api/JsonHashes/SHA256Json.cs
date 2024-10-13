60e3da310754ad01db49d468b14a7188fd834b58
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2024-10-13 11:16:13 -0300

    Created initial structure and test case to compute hash from custom object

---
+++ src/AspNetJsonHash.Api/JsonHashes/SHA256Json.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace AspNetJsonHash.Api.JsonHashes;
+
+internal class SHA256Json<T>
+{
+    internal string Compute(T customObject)
+    {
+        var json = JsonSerializer.Serialize(customObject);
+        var hash = SHA256.HashData(Encoding.UTF8.GetBytes(json));
+        var hashText = new StringBuilder();
+
+        for (var i = 0; i < hash.Length; i++)
+            hashText.Append($"{hash[i]:X2}");
+
+        return hashText.ToString();
+    }
+}
---
+++ tests/AspNetJsonHash.Tests/JsonHashes/SHA256JsonTests.cs
@@ -0,0 +1,25 @@
+﻿using AspNetJsonHash.Api.JsonHashes;
+using FluentAssertions;
+
+namespace AspNetJsonHash.Tests.JsonHashes;
+
+public class SHA256JsonTests
+{
+    [Fact]
+    public void GivenCustomObject_WhenComputeHash_ThenReturnItsHashAsSHA256()
+    {
+        var myObject = new
+        {
+            Name = "Leandro Yuri Joaquim Rodrigues",
+            Age = 41,
+            Address = "Rua Furnas",
+            Document = "69062468870"
+        };
+        var expectedHash = "6314107265BFA86D104F6C632F36364164DC39637891C6B1C324712D312B0331";
+        var jsonHash = new SHA256Json<dynamic>();
+
+        var hash = jsonHash.Compute(myObject);
+
+        hash.Should().Be(expectedHash);
+    }
+}
